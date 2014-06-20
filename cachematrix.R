deb5d2fab7039b81007fff87ddbdb662fae02ab8
Thomas E <spmte@runbox.com>
2014-06-20 23:01:51 +0200

    Solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Based on the example from Programming Assignement #2
+## Contains two functions:
+##   - makeCacheMatrix : will create a "cache-friendly" matrix
+##   - cacheSolve      : will "solve" the matrix and cache the result
 
-## Write a short comment describing this function
+## Creates a cache-friendly matrix
+## Example: m = makeCacheMatrix(matrix(c(4, 2, 7, 6), 2, 2))
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This will solve the matrix and cache the result
+## Example: m = makeCacheMatrix(matrix(c(4, 2, 7, 6), 2, 2))
+##          cacheSolve(m)
+##          cacheSolve(m) # should produce the cached message
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
